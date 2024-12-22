78e5cf46fca700d6a2f072a046dc7fde31660de2
Mehebul02 <mehebul2123@gmail.com>
2024-12-22 17:05:01 +0600

    Global jwt token cretaed successfully

--- src/app/middleWares/auth.ts
+++ src/app/middleWares/auth.ts
@@ -1,8 +1,9 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { NextFunction, Request } from "express"
 import catchAsync from "../utils/catchAsync"
 import AppError from "../errors/appError";
 import httpStatus from "http-status";
+import { User } from '../../modules/user/user.model';
 
 
 const auth = (RequiredRole: string) => {
@@ -12,7 +13,20 @@ const auth = (RequiredRole: string) => {
             throw new AppError(httpStatus.UNAUTHORIZED, 'Token not found')
         }
 
-        const decoded = jwt.verify(token,'secrete')
+        const decoded = jwt.verify(token, 'secrete') as JwtPayload
+
+        const { role, email } = decoded
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            throw new AppError(httpStatus.UNAUTHORIZED, 'User not found')
+        }
+
+        if (RequiredRole !== role) {
+            throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized")
+        }
+
+        req.user = decoded as JwtPayload
 
     })
 }
