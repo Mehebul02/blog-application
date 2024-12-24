42f8faf8dd9ac1a85ad9866d6c3205109211fa8c
Mehebul02 <mehebul2123@gmail.com>
2024-12-24 23:21:18 +0600

    Filter added

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -19,4 +19,38 @@ class QueryBuilder<T> {
         }
         return this
     }
+
+    filter() {
+        const queryObj = { ...this.query }
+
+        // filtering
+        const excludeField = ["searchTerm", "sort", "limit", 'page', "fields"]
+
+        excludeField.forEach((el) => delete queryObj[el])
+        this.modelQuery = this.modelQuery.find(queryObj)
+        return this
+    }
+
+    sort() {
+        const sort = (this.query.sort as string)?.split(',').join(' ') || "-createdAt"
+        this.modelQuery = this.modelQuery.sort(sort as string)
+        return this
+    }
+
+    paginate() {
+        const page = Number(this?.query.page) || 1
+        const limit = Number(this.query.limit) || 10
+
+        const skip = (page - 1) * limit
+        this.modelQuery = this.modelQuery.skip(skip).limit(limit)
+        return this
+    }
+
+    fields(){
+        const fields =(this.query.fields as string)?.split(',').join(' ') || '-__v'
+        this.modelQuery = this.modelQuery.select(fields)
+        return this
+    }
 }
+
+export default QueryBuilder
